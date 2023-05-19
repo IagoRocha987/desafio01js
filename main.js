2845e9bf4538db7622ea650da2c4f5ee9baf4a3b
IagoRocha987 <116189970+IagoRocha987@users.noreply.github.com>
2023-05-19 08:54:08 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function ativarNome(){
+    let nome = 'Sávio';
+    console.log(nome);
+    document.getElementById('nomes').innerHTML=nome
+}
+
+
+function somarNumeros(){
+    let a = 5;
+    let b = 10;
+    let c = a+b;
+    
+    console.log(c);
+
+    document.getElementById('resultado').innerHTML=c
+}
